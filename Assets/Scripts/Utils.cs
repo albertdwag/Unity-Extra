b99ae132e87495318d56e99c92c7b681c046fe58
albertdwag <albertcesar463@gmail.com>
2023-03-30 08:50:05 -0300

    creating an extension script.

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils  
+{
+    public static void Scale(this Transform t, float size = 1.2f)
+    {
+        t.localScale = Vector3.one * size;
+    }
+
+
+}
